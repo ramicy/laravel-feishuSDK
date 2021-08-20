bde7f4d0882f3c696e8ba3b603690e14b3561ae4
ramicy <1064005385@qq.com>
2021-08-20 10:10:29 +0800

    fix redirect_uri

--- src/Utils/AuthUtil.php
+++ src/Utils/AuthUtil.php
@@ -7,7 +7,7 @@ class AuthUtil
     public static function gotoLoginPage() {
         $query = http_build_query([
             'app_id' => config('feishu.appId'),
-            'redirect_uri' => config('feishu.redict_url')
+            'redirect_uri' => rtrim(config('feishu.redict_url'), '/') . '/api/feishu/code2session'
         ]);
 
         return redirect('https://open.feishu.cn/open-apis/authen/v1/index?'.urlencode($query));
