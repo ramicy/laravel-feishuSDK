0a72f04e7a79bae57fda3a588b3428a4e83fe37e
ramicy <1064005385@qq.com>
2021-08-20 17:30:16 +0800

    update

--- src/Config/feishu.php
+++ src/Config/feishu.php
@@ -3,6 +3,5 @@
 return [
     'appId' => env('FEISHU_APPID', ''),
     'appSecret' => env('FEISHU_APPSECRET', ''),
-    'chatId_dev' => env('FEISHU_CHATID', ''),
     'redict_url' => env('FEISHU_REDICT_URL', '')
 ];
